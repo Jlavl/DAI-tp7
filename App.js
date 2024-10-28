1d767135ab8e2ad1a2f9d8bd255186cddcf2d0cc
Jlavl <jelaverallauro@gmail.com>
2024-10-28 09:31:16 -0300

    prueba

--- App.js
+++ App.js
@@ -1,26 +1,47 @@
+import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-const Stack = createStackNavigator();
+/* https://reactnavigation.org/docs/hello-react-navigation */
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+const Stack = createNativeStackNavigator();
+
+function LoginScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Login Screen</Text>
+    </View>
+  );
+}
+function ResgisterScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Login Screen</Text>
+    </View>
+  );
+}
+
 const App =() => {
   return (
-
-  <NavigationContainer>
-    <Stack.Navigator initialRouteName="LoginScreen">
-      <Stack.Screen 
-       name="Home"
-       component={HomeScreen}
-       options={{title: 'welcome'}}
-      />
-      <Stack.Screen name="Login" component={LoginScreen}/>
-      <Stack.Screen name="Registrar" component={ResgisterScreen}/>
-
-    </Stack.Navigator>
-  </NavigationContainer>
-
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="LoginScreen">
+        <Stack.Screen 
+         name="Home"
+         component={HomeScreen}
+         options={{title: 'welcome'}}
+        />
+        <Stack.Screen name="Login" component={LoginScreen}/>
+        <Stack.Screen name="Registrar" component={ResgisterScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -32,3 +53,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+ export default App;
